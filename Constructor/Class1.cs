abb60a8f490c701851dc948fdca2385434c6b2a1
SanthiK21 <SanthiK@fixitytech.com>
2022-11-22 15:47:05 +0530

    c sharp

---
+++ Constructor/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Class1
+    {
+        class paraconstrctor
+        {
+            public int a, b;
+            public paraconstrctor(int x, int y)  // decalaring Paremetrized Constructor with ing x,y parameter
+            {
+                a = x;
+                b = y;
+            }
+        }
+    }
+}
